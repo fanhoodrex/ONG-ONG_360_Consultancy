31668caeceba85d5043b864555e84e4eec205e4f
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2019-11-05 14:23:59 +0800

    Update 04.py

--- Fang/04.py
+++ Fang/04.py
@@ -6,15 +6,16 @@ Created on Fri Nov  1 12:37:02 2019
 """
 day_list = [i+1 for i in range(7)]
 
+
 def weekday(): 
     """calcualte the price based on duration"""
-    if hour == 0 and minute < 15: 
+    if hour == 0 and minute <= 15: 
         amount = 0 #Exit Within 5 minutes are free
     elif 0 < hour <= 3:
         amount = 3
     else:
         amount = hour
-    if minute > 5: # tolerate 5 minutes & calculate price based on minute
+    if hour != 0 and minute > 5: # tolerate 5 minutes & calculate price based on minute
         amount += 1
     else:
         pass
@@ -22,13 +23,13 @@ def weekday():
 
 def weekend(): 
     """calcualte the price based on duration"""
-    if hour == 0 and minute < 15: 
+    if hour == 0 and minute <= 15: 
         amount = 0 #Exit Within 15 minutes are free
     elif 0 < hour <= 2:
         amount = 5
     else:
         amount = 5 + 2*(hour - 2)
-    if minute > 5: # tolerate 5 minutes & calculate price based on minute
+    if hour != 0 and minute > 5: # tolerate 5 minutes & calculate price based on minute
         amount += 2
     else:
         pass
@@ -36,22 +37,22 @@ def weekend():
 
 
 while True:
-    day = int(input("Enter the day:"))
+    day = float(int(input("Enter the day:")))
     duration = input("Enter the Duration:")
 
     hour = int(duration.split(":")[0]) # slice out the hour part
     minute = int(duration.split(":")[1]) # slice out the minute part 
-    amount = 0 #initiative 
+    amount = 0.0 #initiative 
     
     if day in day_list[:5]:
         amount = weekday()
         if amount > 20:
-            amount = 20
+            amount = 20.00
         print(f"\nDuration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM")
 
     elif day in day_list[5:]:
         amount = weekend()
         if amount > 40:
-            amount = 40
+            amount = 40.00
         print(f"\nDuration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM")
         
