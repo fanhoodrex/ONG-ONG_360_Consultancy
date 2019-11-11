df90429e056a89aab96f27b09156d2b2f1a4645c
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2019-11-11 14:11:18 +0800

    Update the 04.py

--- Fang/04.py
+++ Fang/04.py
@@ -1,43 +1,49 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  1 12:37:02 2019
-
-@author: Zac_Fang
-"""
 import time
 
 day_list = list(range(1,8))
+enter_count = 1
 
 def day_input_eva():
+    "this function is mainly for validating user's input on day chosen "
+    global enter_count
     while True:        
         try:
-            day = int(input("Enter the day number between 1-7:")) # string
+            day = int(input(str(enter_count)+".Enter the day number between 1-7:")) # string
             if day != int and day not in day_list: # condition on type 
-                print("input is out of day_list scope")
+                print("input is out of scope,pls enter again")
                 time.sleep(1.5)
+                enter_count += 1 
                 continue
             else:
+                enter_count += 1
                 break
         except ValueError:
-            print("pls enter correct integer number")
+            print("pls enter correct integer format number,enter again")
             time.sleep(1.5)
+            enter_count += 1 
             continue
     return day
 
 def time_input_eva():
+    "this function is mainly for validating user's input on duration chosen"
     while True:
         try:
             duration = input("Enter the Duration:")
             hour = int(duration.split(":")[0]) # slice out the hour part
             minute = int(duration.split(":")[1]) # slice out the minute part
             if type(hour) != int or type(minute) != int : # condition on type 
-                print("pls enter correct format.....")
+                print("pls enter correct (Hours:Minutes HH:MM format) format.....")
                 time.sleep(1.5)
                 continue
             else:
+                # in case if user enter minute value > 60 
+                if (minute//60) != 0:
+                    hour += (minute//60) # add to hour 
+                    minute %= 60 # reduce the minute
                 break
         except ValueError:
-            print("ValueError,pls enter correct integer format......")
+            print("ValueError,pls enter correct (Hours:Minutes HH:MM format) format......")
             time.sleep(1.5)
             continue
         except IndexError:
@@ -46,57 +52,43 @@ def time_input_eva():
             continue
     return hour,minute
 
-def weekday(): 
-    """calcualte the price based on duration"""
-    if hour == 0:
-        if minute <= 10:
-            amount = 0 #Exit Within 5 minutes are free
+def week(*args): # enter the how many day are considered as weekday,return as tuple
+    "calculate the amount based on the date and hours"
+    def inner(fir_hours,fir_charge,sub_charge,min_free,tol_min):
+        if 0 <= hour < fir_hours:
+            amount = fir_charge
+            if hour == 0 and minute <= min_free: #within minutes are free
+                amount = 0 
         else:
-            amount = 3
-    elif 0 < hour < 3:
-        amount = 3
+            # expression amount = 5 + (hour-2)*2 + 2
+            amount = fir_charge + (hour-fir_hours) * sub_charge
+            if minute > tol_min:
+                amount -= sub_charge            
+        return amount
+    # if the day user enter is on weekday or weekend 
+    if day in args:
+        print("\nHello Weekday")
+        amount = inner(3,3,1,15,5)
+        # the order of parameters is fir_hours,fir_charge,sub_charge,min_free,tol_min
+        if amount > 20:
+            amount = 20.00
     else:
-        if minute <= 5:
-            amount = hour
-        else:
-            amount = hour + 1.5
+        print("\nHello Weekend")
+        amount = inner(2,5,2,15,5)
+        # the order of parameters is fir_hours,fir_charge,sub_charge,min_free,tol_min
+        if amount > 40:
+            amount = 40.00
     return amount
 
-def weekend(): 
-    """calcualte the price based on duration"""
-    if hour == 0:
-        if minute <= 10:
-            amount = 0 #Exit Within 5 minutes are free
-        else:
-            amount = 5
-    elif 0 < hour < 2:
-        amount = 5
-    else:
-        if minute <= 5:
-            amount = 5 + (hour - 2) * 2
-            # expression equals to amount = 2 hour + 1         
-        else:
-            amount = 5 + (hour - 2) * 2 + 2
-            # expression equals to amount =  2 * hour + 3 
-    return amount
-
+#below is the main function 
 while True:
     day = day_input_eva()
     hour,minute = time_input_eva()
     amount = 0.0 #initiative the amount
-
-    if day in day_list[:5]:
-        amount = weekday()
-        if amount > 20:
-            amount = 20.00
-        print(f"Duration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM",end="\n")
-
-    elif day in day_list[5:]:
-        amount = weekend()
-        if amount > 40:
-            amount = 40.00
-        print(f"Duration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM",end="\n")
+    amount = week(1,2,3,4,5)
+    print(f"Duration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM\n")
 
 
 
-
+a = list(range(1,11))
+b = ["一","二","三","四","五","六","七"]
