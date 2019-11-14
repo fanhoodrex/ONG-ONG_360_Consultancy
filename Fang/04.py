e717ef0a60949d59e816fe8437bcb4a8ec1cf1bb
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2019-11-14 19:11:54 +0800

    Update

--- Fang/04.py
+++ Fang/04.py
@@ -1,20 +1,26 @@
+"""
+this test1 is used to test the computing part on the amount 
+based on iteration and while loop 
+"""
+
 import time
 # define the price dictionary for each different day
 # [ first 3 hour , RM3, Subsequent hour 1 , Exit 15 mins are free , Tolerate 5 min,Max charge RM]
 price_dict = {
-    1:[3,3,1,15,5,20],
-    2:[3,3,1,15,5,20],
-    3:[3,0,1,15,5,20], # requirement change as first 3 hour cost 0 RM 
-    4:[3,3,1,15,5,20],
-    5:[3,3,1,15,5,20],
-    6:[2,5,2,15,5,40],
-    7:[2,5,2,15,5,40],
+    1:[3,3,60,1,15,5,20],
+    2:[3,3,60,1,15,5,20],
+    3:[3,0,30,1,15,5,20], # requirement change as first 3 hour cost 0 RM 
+    4:[3,3,60,1,15,5,20],
+    5:[3,3,60,1,15,5,20],
+    6:[2,5,60,2,15,5,40],
+    7:[2,5,60,2,15,5,40],
 }
-
+# the order of parameters is fir_hours,fir_charge,sub_charge,min_free,tol_min
+enter_count = 1
 def day_input_eva():
     "this function is mainly for validating user's input on day chosen "
+    global enter_count
     day_list = list(range(1,8))
-    enter_count = 1
     while True:        
         try:
             day = int(input(str(enter_count)+".Enter the day number between 1-7:")) # string
@@ -26,7 +32,7 @@ def day_input_eva():
             else:
                 enter_count += 1
                 break
-        except ValueError:
+        except Exception:
             print("pls enter correct integer format number,enter again")
             time.sleep(1.5)
             enter_count += 1 
@@ -46,53 +52,48 @@ def time_input_eva():
                 continue
             else:
                 # in case if user enter minute value > 60 
-                if (minute//60) != 0:
+                if (minute//60) != 0: 
                     hour += (minute//60) # add to hour 
                     minute %= 60 # reduce the minute
+                total_minutes = hour * 60 + minute # conver the HH:MM format to minutes
                 break
-        except ValueError:
-            print("ValueError,pls enter correct (Hours:Minutes HH:MM format) format......")
+        except (IndexError,ValueError):
+            print("pls enter correct (Hours:Minutes HH:MM format) format......")
             time.sleep(1.5)
             continue
-        except IndexError:
-            print("IndexError,pls enter (Hours:Minutes HH:MM format)......")
-            time.sleep(1.5)
-            continue
-    return hour,minute
+    return hour,minute,total_minutes
 
-def week(): # enter the how many day are considered as weekday,return as tuple
+def week(hour,minute,total_minutes): # enter the how many day are considered as weekday,return as tuple
     "outer function that return the amount value"
-    def inner(day):
-        """inner function that calculate the amount based on the date and hours"""
-        fir_hours = price_dict[day][0] #get the first hour amount from dictionary
-        fir_charge = price_dict[day][1] #get the first charge amount from dictionary
-        sub_charge = price_dict[day][2] #get the subsequent charge amount from dictionary
-        min_free = price_dict[day][3] #get the minimum minutes free from dictionary
-        tol_min = price_dict[day][4] #get the tolerate minutes from dictionary
-
-        if 0 <= hour < fir_hours: # first few hours free
-            amount = fir_charge
-            if hour == 0 and minute <= min_free: #within minutes are free
-                amount = 0 
-        else:
-            # subsequent hours charge calculation 
-            amount = fir_charge + (hour - fir_hours) * sub_charge
-            if minute > tol_min: # maximum charge
-                amount += sub_charge            
-        return amount
-
-    amount = inner(day)
-    # the order of parameters is fir_hours,fir_charge,sub_charge,min_free,tol_min
-    if amount > max_charge:
-        amount = max_charge
+    fir_hours = price_dict[day][0] #get the first hour amount from dictionary
+    fir_charge = price_dict[day][1] #get the first charge amount from dictionary
+    sub_min = price_dict[day][2] #get the subsequent min from dictionary
+    sub_charge = price_dict[day][3] #get the subsequent charge amount from dictionary
+    min_free = price_dict[day][4] #get the minimum minutes free from dictionary
+    tol_min = price_dict[day][5] #get the tolerate minutes from dictionary
+        
+    # 3 layers nested if else statement
+    if 0 <= total_minutes <= min_free:#first 15 min free
+        amount = 0
+    elif min_free < total_minutes <= 60 * fir_hours: # first 3 hours free 3:05 = 180 minutes
+        amount = fir_charge
+    else:# time is 3 hours above and amount charge based on minute unit
+        minute = total_minutes - (fir_hours * 60)
+        amount = fir_charge + (hour - fir_hours) * sub_charge # compute the amount based on the hour unit
+        print(amount)
+        print(minute)
+        while minute > tol_min:
+            amount += sub_charge # increase the subsequent charge each iteration
+            minute -= sub_min # substract the subsequent minute each iteration
+        if amount > max_charge:
+            amount = max_charge # reassignment if exceed the maximum charge
     return amount
     
 #below is the main function 
+#if __name__ == "__main__":
 while True:
     day = day_input_eva()
     max_charge = price_dict[day][-1] # get the maximum charge from the price dictionary
-    hour,minute = time_input_eva()
-    amount = week()
-    print(f"Duration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM\n")
-
-
+    hour,minute,total_minutes = time_input_eva()
+    amount = week(hour,minute,total_minutes)
+    print(f"Total Minutes:{total_minutes}\nDuration: {hour} Hours {minute} Minutes\nNet Amount Needed To Paid: {amount} RM\n")
