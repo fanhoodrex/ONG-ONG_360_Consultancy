2ab2c5ffda0d2aca29261f5d1ba63c5d4328253a
Zachary.R.Fang <40188521+fanhoodrex@users.noreply.github.com>
2019-12-09 11:34:22 +0800

    Compete the first 10 currency output
    compete first search with less than or euqal to 10 currency requests
    
    output is dictionary with keys [base,date,rates:{key,value}]

--- Crawler/test/test.py
+++ Crawler/test/test.py
@@ -30,7 +30,6 @@ def Crawl_Data(date,base_currency,list_quote):
         "quote_currency_9":None
     }
 
-    result_dict = {}
     def append_params(chunk):
         """append the quote currency from the list to the params"""
         n = 0
@@ -43,7 +42,7 @@ def Crawl_Data(date,base_currency,list_quote):
     def request_json():
         respone = requests.get(url,headers=header,params=params)
         res_dict = json.loads(respone.text) #convert json string to Python data type
-        with open("json1.json","w") as f:
+        with open("res_dict1.json","w") as f:
             str_json = json.dumps(res_dict,sort_keys=True,indent=None)
             f.write(str_json)
         return res_dict
@@ -52,17 +51,12 @@ def Crawl_Data(date,base_currency,list_quote):
         while len(list_quote) > 0:
             chunk = list_quote[:10] # slice the first batch of 10 currency
             params = append_params(chunk) # fill the params with chunk of 10 currency
-            print("ok1")
             res_dict = request_json() # send the request and return json dictionary
-            print("ok2")
-            result_dict['widget'] = res_dict["widget"]
-            print("ok3")
-            print(result_dict)
+            result_dict['rates'] = res_dict["widget"]
             # append widget key value to the widget dictionary during each loop
-            print("ok4")
             list_quote = list_quote[10:] # remove the first batch of 10 currency
             time.sleep(1)
-            # list comprehension 
+
             """
             quoteCurrency = [sub['quoteCurrency'] for sub in res_dict["widget"]]
             average_rate = [sub['average'] for sub in res_dict["widget"]]
@@ -75,9 +69,13 @@ def Crawl_Data(date,base_currency,list_quote):
     except Exception:
         print("request failed, error:")
 
-date = "2019-12-04"
+search_date = "2019-12-04"
 base_currency = 'MYR'
 list_quote = ['CNY','USD','GBP','SGD']
 
-result_dict = Crawl_Data(date,base_currency,list_quote)
+result_dict = {'base':base_currency,'date':search_date} # initialize
+result_dict = Crawl_Data(search_date,base_currency,list_quote) # add the rates key/values to result dict
+
 print(result_dict)
+print("\n")
+print(result_dict['rates'])
